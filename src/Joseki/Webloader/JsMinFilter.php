b710f4341fb52fb476f1d94484bfa2027e111c95
Filip Procházka <filip@prochazka.su>
2015-10-11 21:41:33 +0200

    JsMinFilter: do not minify minified files

--- src/Joseki/Webloader/JsMinFilter.php
+++ src/Joseki/Webloader/JsMinFilter.php
@@ -3,16 +3,23 @@
 namespace Joseki\Webloader;
 
 use JShrink\Minifier;
+use WebLoader\Compiler;
 
 class JsMinFilter
 {
     /**
      * Minify target code
      * @param string $code
+     * @param Compiler $compiler
+     * @param string $file
      * @return string
      */
-    public function __invoke($code)
+    public function __invoke($code, Compiler $compiler, $file)
     {
+        if (strpos($file, '.min.') !== FALSE) {
+            return $code;
+        }
+
         return Minifier::minify($code);
     }
 }
